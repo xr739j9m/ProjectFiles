04b670a62d29e6db64eaa9af79ec49bb49163e0a
xr739j9m <av956797@gmail.com>
2021-10-16 19:41:42 +0800

    added another change

--- OOP_Proj/src/inheritance/Person.java
+++ OOP_Proj/src/inheritance/Person.java
@@ -3,5 +3,6 @@ package inheritance;
 public class Person {
     public static void main(String[] args) {
         System.out.println("This is a test for Github");
+        System.out.println("wassup");
     }
 }
